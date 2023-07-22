e12a8e9481549e6da93d8f81cfa83fc5f74b9c26
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-22 20:07:28 +0545

    Task can now be added

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 function App() {
@@ -17,6 +18,12 @@ function App() {
         "reminder": true
       }
 ])
+//Add task
+const addTask = (task) =>{
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
   
 //Delete Task
 const deleteTask=(id)=>{
@@ -33,6 +40,7 @@ setTasks(tasks.map((task) => task.id === id
   return (
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
          <Tasks tasks={tasks} 
          onDelete={deleteTask} 
