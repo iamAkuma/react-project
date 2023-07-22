8c9a851b7a2e79bacdbb2208aab24ac0722198bb
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-22 19:39:22 +0545

    Task Reminder added

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -18,10 +18,28 @@ function App() {
       }
 ])
   
+//Delete Task
+const deleteTask=(id)=>{
+setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle Reminder
+const toggleRem = (id) => {
+setTasks(tasks.map((task) => task.id === id 
+?
+ {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+         <Tasks tasks={tasks} 
+         onDelete={deleteTask} 
+         onToggle={toggleRem} />
+         ) 
+      :
+       ('No Tasks Added.')}
     </div>
   );
 }
