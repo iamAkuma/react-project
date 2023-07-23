1c1efc5566291daf0cdbd676f263c187070067df
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-23 17:26:54 +0545

    Task added through a backend json server library

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,54 +1,57 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [showAdd, setshowAdd] = useState(false)
-  const [tasks, setTasks] =useState([
-    {
-        "id": 1,
-        "text": "Doctors Appointment",
-        "day": "Feb 5th at 2:30pm",
-        "reminder": true
-      },
-      {
-        "id": 2,
-        "text": "Meeting at School",
-        "day": "Feb 6th at 1:30pm",
-        "reminder": true
-      }
-])
-//Add task
-const addTask = (task) =>{
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
-}
-  
-//Delete Task
-const deleteTask=(id)=>{
-setTasks(tasks.filter((task) => task.id !== id))
-}
+  const [tasks, setTasks] = useState([])
 
-//Toggle Reminder
-const toggleRem = (id) => {
-setTasks(tasks.map((task) => task.id === id 
-?
- {...task, reminder: !task.reminder} : task))
-}
+  useEffect(() => {
+    const getTasks = async () => {
+      const taksFromServer = await fetchTasks()
+      setTasks(taksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleRem = (id) => {
+    setTasks(tasks.map((task) => task.id === id
+      ?
+      { ...task, reminder: !task.reminder } : task))
+  }
 
   return (
     <div className="container">
-      <Header onAdd={()=> setshowAdd(!showAdd)}/>
-      {showAdd && <AddTask onAdd={addTask}/>}
+      <Header onAdd={() => setshowAdd(!showAdd)} show={showAdd} />
+      {showAdd && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
-         <Tasks tasks={tasks} 
-         onDelete={deleteTask} 
-         onToggle={toggleRem} />
-         ) 
-      :
-       ('No Tasks Added.')}
+        <Tasks tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleRem} />
+      )
+        :
+        ('No Tasks Added.')}
     </div>
   );
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title, onAdd}) => {
+const Header = ({title, onAdd, show}) => {
 
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color= 'green' text= 'Add' onClick={onAdd}/>
+        <Button color={show? 'red' : 'green'} 
+        text= {show ? 'Close' : 'Add'} onClick={onAdd}/>
          </header>
   )
 }
