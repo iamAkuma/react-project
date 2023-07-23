af051025e2372e554cbc138cf88619d4dc1b5651
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-23 17:46:53 +0545

    everything connected to backend

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -22,23 +22,66 @@ function App() {
 
     return data
   }
+  //Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
   //Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
+  // const addTask = (task) => {
+  //   const id = Math.floor(Math.random() * 10000) + 1
+  //   const newTask = { id, ...task }
+  //   setTasks([...tasks, newTask])
+  // }
+
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   //Toggle Reminder
-  const toggleRem = (id) => {
-    setTasks(tasks.map((task) => task.id === id
+  const toggleRem = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      }
+    )
+
+      const data = await res.json()
+
+    setTasks(tasks.map((task) => 
+    task.id === id
       ?
-      { ...task, reminder: !task.reminder } : task))
+      { ...task, reminder: data.reminder } : task))
   }
 
   return (
